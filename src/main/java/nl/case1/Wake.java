d706bd9714d19a2aeb130d3b1dbad346b51051d5
Peter Hendriks <phendriks@servoy.com>
2019-05-23 11:40:25 +0200

    dunno why doesnt work

--- src/main/java/nl/case1/Wake.java
+++ src/main/java/nl/case1/Wake.java
@@ -9,6 +9,7 @@ import java.awt.AWTException;
 import java.awt.MouseInfo;
 import java.awt.PointerInfo;
 import java.awt.Robot;
+import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -18,22 +19,31 @@ import java.util.logging.Logger;
  */
 public class Wake {
 
-    public Wake() {
-        Robot robot;
+    public Wake(int interval) {
 
-        try {
-            robot = new Robot();
-            while (true) {                
-                PointerInfo pointerInfo = MouseInfo.getPointerInfo();
-                robot.mouseMove(pointerInfo.getLocation().x + 1,
-                        pointerInfo.getLocation().y + 1);
-                Thread.sleep(20000);
+        while (true) {
+
+            try {
+                
+                int x = MouseInfo.getPointerInfo().getLocation().x + 10;
+                int y = MouseInfo.getPointerInfo().getLocation().y + 10;
+
+                new Robot().mouseMove(x,y);
+
+                System.out.println("Wake! "
+                        + new Date()
+                        + " moved mouse to new location: "
+                        + x 
+                        +", "
+                        +y);
+
+                Thread.sleep(interval);
+                
+            } catch (Exception e) {
+                e.printStackTrace();
             }
 
-        } catch (AWTException ex) {
-            Logger.getLogger(Wake.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (InterruptedException ex) {
-            Logger.getLogger(Wake.class.getName()).log(Level.SEVERE, null, ex);
         }
+
     }
 }
